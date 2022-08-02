04b68918d41e610a2417604c39af7fb762ad7500
EC2 Default User <ec2-user@ip-172-31-43-247.us-east-2.compute.internal>
2022-08-02 06:36:46 +0000

    this is my 3nd commit

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk -y
+
+sudo yum install java -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
